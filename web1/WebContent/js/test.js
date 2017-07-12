0bc4b4183743b1d62b501d3d92202866bd803f24
imhyejin <imagine.codes@gmail.com>
2017-07-12 18:19:19 +0900

    sdadsd

---
+++ web1/WebContent/js/test.js
@@ -0,0 +1,30 @@
+/**
+ * 뭐냐 ~~~~
+ */
+
+
+function checkValue(fObj){
+	var result ="";
+	
+	for(var i =0; i<fObj.elements.length;i++){
+		if(fObj.elements[i].value!="전송"){
+			result+=fObj.elements[i].value;
+		}
+		if (i%2==1&&fObj.elements[i].value!="" ){
+			var checknum = new Number(fObj.elements[i].value);
+				
+			if (isNaN(checknum)){
+				alert("숫자 입력하라고!");
+				fObj.elements[i].value="";
+				fObj.elements[i].focus();
+			return false;
+			}				
+		}
+		
+		if(i==fObj.elements.length-1){
+			fObj.elements[i].value=result;
+
+		}
+	}
+	return false;
+}
