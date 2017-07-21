319b6aecfadfcb088bf166e5a1f48620515560de
imhyejin <imagine.codes@gmail.com>
2017-07-21 15:43:49 +0900

    4223

--- web1/src/com/test/service/UserService.java
+++ web1/src/com/test/service/UserService.java
@@ -5,31 +5,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.test.common.DBConn;
+import com.test.dto.UserInfo;
 
 public class UserService {
 
-	public boolean insertUser(HashMap<String, String> hm) {
+	public boolean insertUser(UserInfo ui) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
 			con = DBConn.getCon();
-			String sql = "insert into user_info(userid,userpwd,username,address,hp1,hp2,hp3,age) ";
+			String sql = "insert into user_info(userid,userpwd,username,age,address,hp1,hp2,hp3) ";
 			sql += "values (?,?,?,?,?,?,?,?)";
 			ps = con.prepareStatement(sql);
 
-			ps.setString(1, hm.get("userid"));
-			ps.setString(2, hm.get("userpwd"));
-			ps.setString(3, hm.get("username"));
-			ps.setString(4, hm.get("address"));
-			ps.setString(5, hm.get("hp1"));
-			ps.setString(6, hm.get("hp2"));
-			ps.setString(7, hm.get("hp3"));
-			ps.setString(8, hm.get("age"));
+			ps.setString(1, ui.getUserId());
+			ps.setString(2, ui.getUserPwd());
+			ps.setString(3, ui.getUserName());
+			ps.setInt(4, ui.getAge());
+			ps.setString(5, ui.getAddress());
+			ps.setString(6, ui.getHp1());
+			ps.setString(7, ui.getHp2());
+			ps.setString(8, ui.getHp3());
+
 			int result = ps.executeUpdate();
 			if (result == 1) {
 				con.commit();
@@ -48,7 +48,7 @@ public class UserService {
 		return false;
 	}
 
-	public boolean deleteUser(HashMap<String, String> hm) {
+	public boolean deleteUser(UserInfo ui) {
 		Connection con = null;
 		PreparedStatement ps = null;
 
@@ -56,7 +56,7 @@ public class UserService {
 			con = DBConn.getCon();
 			String sql = "delete from user_info where usernum= ?";
 			ps = con.prepareStatement(sql);
-			ps.setString(1, hm.get("deleteNum"));
+			ps.setInt(1, ui.getUserNum());
 			int result = ps.executeUpdate();
 			if (result == 1) {
 				con.commit();
@@ -75,7 +75,7 @@ public class UserService {
 		return false;
 	}
 
-	public boolean updateUser(HashMap<String, String> hm) {
+	public boolean updateUser(UserInfo ui) {
 		Connection con = null;
 		PreparedStatement ps = null;
 
@@ -84,10 +84,10 @@ public class UserService {
 			String sql = "update user_info set username=?, userid=?, age=? where usernum= ?";
 			ps = con.prepareStatement(sql);
 
-			ps.setString(1, hm.get("username"));
-			ps.setString(2, hm.get("userid"));
-			ps.setString(3, hm.get("age"));
-			ps.setString(4, hm.get("usernum"));
+			ps.setString(1, ui.getUserName());
+			ps.setString(2, ui.getUserId());
+			ps.setInt(3, ui.getAge());
+			ps.setInt(4, ui.getUserNum());
 
 			int result = ps.executeUpdate();
 
@@ -108,58 +108,58 @@ public class UserService {
 		return false;
 	}
 
-//	public List<Map> searchUser(HashMap<String, String> hm) {
-//		Connection con = null;
-//		PreparedStatement ps = null;
-//		try {
-//			con = DBConn.getCon();
-//			String sql = "select id, pwd, name,class_num,age from user_info";
-//			if (!hm.get("name").equals("")) {
-//				sql += " WHERE name like ?";
-//			}
-//			ps = con.prepareStatement(sql);
-//			if (!hm.get("name").equals("")) {
-//				ps.setString(1, hm.get("name"));
-//			}
-//			ResultSet rs = ps.executeQuery();
-//			ArrayList list = new ArrayList();
-//			while (rs.next()) {
-//				HashMap hm1 = new HashMap();
-//				hm1.put("id", rs.getString("id"));
-//				hm1.put("pwd", rs.getString("pwd"));
-//				hm1.put("name", rs.getString("name"));
-//				hm1.put("class_num", rs.getString("class_num"));
-//				hm1.put("age", rs.getString("age"));
-//				list.add(hm1);
-//			}
-//			return list;
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				ps.close();
-//				DBConn.closeCon();
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		return null;
-//	}
+	// public List<Map> searchUser(HashMap<String, String> hm) {
+	// Connection con = null;
+	// PreparedStatement ps = null;
+	// try {
+	// con = DBConn.getCon();
+	// String sql = "select id, pwd, name,class_num,age from user_info";
+	// if (!hm.get("name").equals("")) {
+	// sql += " WHERE name like ?";
+	// }
+	// ps = con.prepareStatement(sql);
+	// if (!hm.get("name").equals("")) {
+	// ps.setString(1, hm.get("name"));
+	// }
+	// ResultSet rs = ps.executeQuery();
+	// ArrayList list = new ArrayList();
+	// while (rs.next()) {
+	// HashMap hm1 = new HashMap();
+	// hm1.put("id", rs.getString("id"));
+	// hm1.put("pwd", rs.getString("pwd"));
+	// hm1.put("name", rs.getString("name"));
+	// hm1.put("class_num", rs.getString("class_num"));
+	// hm1.put("age", rs.getString("age"));
+	// list.add(hm1);
+	// }
+	// return list;
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// } finally {
+	// try {
+	// ps.close();
+	// DBConn.closeCon();
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// return null;
+	// }
 
-	public String loginUser(HashMap<String, String> hm) {
+	public String loginUser(UserInfo ui) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
 			con = DBConn.getCon();
 			String sql = "select userid, userpwd from user_info WHERE userid=?";
 			ps = con.prepareStatement(sql);
-			ps.setString(1, hm.get("loginId"));
+			ps.setString(1, ui.getUserId());
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
 				String userId = rs.getString("userid");
 				String userpwd = rs.getString("userpwd");
-				return checkPwd(userpwd, hm.get("loginPwd"),userId);
+				return checkPwd(userpwd, ui.getUserPwd(), userId);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -175,46 +175,47 @@ public class UserService {
 	}
 
 	private String checkPwd(String pwd1, String pwd2, String id) {
-		if(pwd1.equals(pwd2)){
-			return "아이디"+id+"님이 로그인 하셨습니다.";
+		if (pwd1.equals(pwd2)) {
+			return "아이디" + id + "님이 로그인 하셨습니다.";
 		}
 		return "비밀번호 틀렸습니다.";
 	}
 
-	public List<Map> selectUser(HashMap<String, String> hm){
+	public List<UserInfo> selectUser(UserInfo ui) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
 			String sql = "select usernum,userid,username,userpwd,age,address,hp1,hp2,hp3 from user_info";
-			if(hm.get("name")!=null){
+			if (ui.getUserName() != null) {
 				sql += " where username like ?";
 			}
 			con = DBConn.getCon();
 			ps = con.prepareStatement(sql);
-			if(hm.get("name")!=null){
-				ps.setString(1, hm.get("name"));
+			if (ui.getUserName() != null) {
+				ps.setString(1, ui.getUserName());
 			}
 			ResultSet rs = ps.executeQuery();
-			List userList = new ArrayList();
-			while(rs.next()){
-				HashMap hm1 = new HashMap();
-				hm1.put("usernum", rs.getString("usernum"));
-				hm1.put("userid", rs.getString("userid"));
-				hm1.put("username", rs.getString("username"));
-				hm1.put("userpwd", rs.getString("userpwd"));
-				hm1.put("age", rs.getString("age"));
-				hm1.put("address", rs.getString("address"));
-				hm1.put("hp1", rs.getString("hp1"));
-				hm1.put("hp2", rs.getString("hp2"));
-				hm1.put("hp3", rs.getString("hp3"));
-				userList.add(hm1);
+			List<UserInfo> userList = new ArrayList<UserInfo>();
+			while (rs.next()) {
+				UserInfo ui1 = new UserInfo();
+				ui1.setUserNum(Integer.parseInt(rs.getString("usernum")));
+				System.out.println(Integer.parseInt(rs.getString("usernum")));
+				ui1.setUserId(rs.getString("userid"));
+				ui1.setUserName(rs.getString("username"));
+				ui1.setUserPwd(rs.getString("userpwd"));
+				ui1.setAge(Integer.parseInt(rs.getString("age")));
+				ui1.setAddress(rs.getString("address"));
+				ui1.setHp1(rs.getString("hp1"));
+				ui1.setHp2(rs.getString("hp2"));
+				ui1.setHp3(rs.getString("hp3"));
+				userList.add(ui1);
 			}
 			return userList;
-		}catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				ps.close();
 				DBConn.closeCon();
--- web1/src/com/test/servlet/UserServlet.java
+++ web1/src/com/test/servlet/UserServlet.java
@@ -2,16 +2,14 @@ package com.test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.dto.UserInfo;
 import com.test.service.UserService;
 
 public class UserServlet extends HttpServlet {
@@ -21,101 +19,80 @@ public class UserServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resq) throws IOException, ServletException {
 		req.setCharacterEncoding("UTF-8");
 		UserService us = new UserService();
+		UserInfo ui = new UserInfo();
+
+		String userNum = req.getParameter("usernum");
+		String userId = req.getParameter("userid");
+		String userPwd = req.getParameter("userpwd");
+		String userName = req.getParameter("username");
+		String address = req.getParameter("address");
+
+		String age = req.getParameter("age");
+		String hp1 = req.getParameter("hp1");
+		String hp2 = req.getParameter("hp2");
+		String hp3 = req.getParameter("hp3");
+
+		if (userNum != null) {
+			ui.setUserNum(Integer.parseInt(userNum));
+		}
+		ui.setUserId(userId);
+		ui.setUserPwd(userPwd);
+		ui.setUserName(userName);
+		ui.setAddress(address);
+		if (age != null) {
+			ui.setAge(Integer.parseInt(age));
+		}
+		ui.setHp1(hp1);
+		ui.setHp2(hp2);
+		ui.setHp3(hp3);
 
 		String command = req.getParameter("command");
-		HashMap hm = new HashMap();
-		List<Map> list = new ArrayList<Map>();
 
 		if (command == null) {
 			return;
 		}
 
 		if (command.equals("SIGNIN")) {
-
-			String userid = req.getParameter("userid");
-			String userpwd = req.getParameter("userpwd");
-			String username = req.getParameter("username");
-			String address = req.getParameter("address");
-			String hp1 = req.getParameter("hp1");
-			String hp2 = req.getParameter("hp2");
-			String hp3 = req.getParameter("hp3");
-			String age = req.getParameter("age");
-
-			System.out.println(userid + "," + userpwd + "," + username + "," + address + ", " + age + "," + "회원가입");
-			hm.put("userid", userid);
-			hm.put("userpwd", userpwd);
-			hm.put("username", username);
-			hm.put("address", address);
-			hm.put("hp1", hp1);
-			hm.put("hp2", hp2);
-			hm.put("hp3", hp3);
-			hm.put("age", age);
-			// us.insertUser(hm);
-			if (us.insertUser(hm)) {
-				doProcess(resq, "저장이 잘 되었구나");
+			System.out.println("id= " + userId + "pwd= " + userPwd + "name= " + userName + "address= " + address + "age= " + age + "hp= " + hp1 + "-" + hp2 + "-" + hp3 + " ★회원가입★ ");
+			if (us.insertUser(ui)) {
+				doProcess(resq, " 회원가입이 되었구나! ");
 			} else {
-				doProcess(resq, "값 입력해야지 회원가입하지 짜식아");
+				doProcess(resq, " 너를 받아줄 수 없구나. ");
 			}
 		} else if (command.equals("삭제")) {
-			String deleteNum = req.getParameter("usernum");
-			System.out.println(deleteNum + "번 삭제한다잉");
-			// us.deleteUser(deleteNum);
-			hm.put("deleteNum", deleteNum);
-			if (us.deleteUser(hm)) {
-				// doProcess(resq, "삭제했다");
-				list.add(hm);
-				// for (Map m : list) {
-				doProcess(resq, "user의 num "+deleteNum + "번 삭제 하엿습니다.");
-				// }
+			System.out.println(userNum + " 번 ★삭제★ ");
+
+			if (us.deleteUser(ui)) {
+				doProcess(resq, "usernum " + userNum + " 번 삭제 하였습니다.");
 			} else {
-				doProcess(resq, "값 입력해야지 삭제하지 짜식아");
+				doProcess(resq, " 삭제할 수 없구나. ");
 			}
+
 		} else if (command.equals("수정")) {
-			String username = req.getParameter("username");
-			String userid = req.getParameter("userid");
-			String age = req.getParameter("age");
-			String usernum = req.getParameter("usernum");
-			System.out.println(usernum + "업데이트");
-			// us.deleteUser(deleteNum);
-			
-			hm.put("username", username);
-			hm.put("userid", userid);
-			hm.put("age", age);
-			hm.put("usernum", usernum);
-			
-			if (us.updateUser(hm)) {
-				list.add(hm);
-				// for (Map m : list) {
-				doProcess(resq, list + "");
-				// }
+			System.out.println(userNum + " 번 ★업데이트★ ");
+
+			if (us.updateUser(ui)) {
+				doProcess(resq, userNum + ", " + userId);
 			} else {
 				doProcess(resq, "값 입력해야지 업데이트하지 짜식아");
 			}
-		}  else if (command.equals("SELECT")) {
-			String name = req.getParameter("username");
-			System.out.println("이름 : " + name);
-			hm = new HashMap();
-			if (name != null && !name.equals("")) {
-				hm.put("name", "%" + name + "%");
+		} else if (command.equals("SELECT")) {
+			System.out.println("이름 : " + userName);
+			if (userName != null && !userName.equals("")) {
+				ui.setUserName("%" + userName + "%");
 			}
-			List<Map> userList  = us.selectUser(hm);
-			String result="번호{/}이름{/}아이디{/}나이{+}";
-			result+="dis{/}en{/}en{/}en{+}";
-			for(Map m : userList){
-				result += m.get("usernum") + "{/}" + m.get("username") + "{/}" + m.get("userid") + "{/}" + m.get("age") + "{+}"; 
+			List<UserInfo> userList = us.selectUser(ui);
+			String result = "번호{/}이름{/}아이디{/}나이{+}";
+			result += "dis{/}en{/}en{/}en{+}";
+			for (int i = 0; i < userList.size(); i++) {
+				result += userList.get(i).getUserNum() + "{/}" + userList.get(i).getUserName() + "{/}"	+ userList.get(i).getUserId() + "{/}" + userList.get(i).getAge() + "{+}";
 			}
-			result = result.substring(0, result.length()-3);
+			result = result.substring(0, result.length() - 3);
 			doProcess(resq, result);
-			
+
 		} else if (command.equals("LOGIN")) {
-			String loginId = req.getParameter("userid");
-			String loginPwd = req.getParameter("userpwd");
-			System.out.println(loginId + "," + loginPwd + " 검색한다.");
-
-			hm.put("loginId", loginId);
-			hm.put("loginPwd", loginPwd);
-
-			doProcess(resq, us.loginUser(hm));
+			System.out.println("id= " + userId + ", pwd= " + userPwd + " 검색한다.");
+			doProcess(resq, us.loginUser(ui));
 		}
 	}
 
